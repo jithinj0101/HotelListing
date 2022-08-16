7e578bde66f71a726ef4483ae930ccb1d3a9d1e8
Jithin J <jithin.j@hloov.com>
2022-08-16 13:18:18 +0530

    Added EntityFramework and DB

---
+++ HotelListing.API/Data/Country.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+
+
+        public virtual IList<Hotel> Hotels { get; set; }
+    } 
+}
+
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,8 +1,18 @@
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Add DbContext and connection string config
+
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options => {
+    options.UseSqlServer(connectionString);
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
